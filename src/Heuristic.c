e9e5710fd25f62eabcf68db3af4818e8df96dc94
Rosario <rosario.cavelli@icloud.com>
2022-08-18 20:51:56 +0200

    .

--- src/Heuristic.c
+++ src/Heuristic.c
@@ -5,6 +5,9 @@
 #include "Heuristic.h"
 
 double Hcoord(Coord coord1, Coord coord2){
+    printf("Coord1 -> c1: %d, c2: %d\n", coord1->c1, coord1->c2);
+    printf("Coord2 -> c1: %d, c2: %d\n", coord2->c1, coord2->c2);
+
     return sqrt(pow(coord1->c1 - coord2->c1, 2) + pow(coord1->c2 - coord2->c2, 2));
 }
 
