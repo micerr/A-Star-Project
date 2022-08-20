01d3c8e78eb40396f26517620b9f1e58de77ec3f
Rosario <rosario.cavelli@icloud.com>
2022-08-20 19:32:51 +0200

    Rewrote sequential A-Star.

--- src/Heuristic.h
+++ src/Heuristic.h
@@ -3,12 +3,8 @@
 
 #include "ST.h"
 
-<<<<<<< HEAD
 int Hcoord(Coord coord1, Coord coord2);
-=======
-double Hcoord(Coord coord1, Coord coord2);
 double Hhaver(Coord coord1, Coord coord2);
 double Hdijkstra(Coord coord1, Coord coord2);
->>>>>>> a8a31231de68da3f92fbd3cb3c84f473924e5e83
 
 #endif
