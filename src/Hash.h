d4d4717a053bffd4b4b1250c8b8fe0c094f62767
Michele Cerra <fbmichele@hotmail.it>
2022-08-29 23:26:32 +0200

    .

---
+++ src/Hash.h
@@ -0,0 +1,17 @@
+#ifndef HASH_H
+#define HASH_H
+
+#include "Graph.h"
+
+typedef struct hash_s *Hash;
+
+Hash HASHinit(Graph G, int numTh, int (*hfunc)(Hash h, int v));
+void HASHfree(Hash h);
+int hash(Hash h, int v);
+int randomHashing(Hash h, int v);
+int multiplicativeHashing(Hash h, int v);
+int zobristHashing(Hash h, int v);
+int abstractStateZobristHashing(Hash h, int v);
+int abstractFeatureZobristHashing(Hash h, int v);
+
+#endif
